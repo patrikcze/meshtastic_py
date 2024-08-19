463d62024b1cf2940b55ca71e860923721ac4b61
Nakladal, Patrik (Brno) CZE <patrik.nakladal@heidelbergcement.com>
2024-08-19 16:55:36 +0200

    fix(get-reply.py): Removing debug

--- get-reply.py
+++ get-reply.py
@@ -12,7 +12,7 @@ import logging
 
 # Set up logging configuration
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
