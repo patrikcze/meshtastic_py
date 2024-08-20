c42a8bfe09903d49f03f1ee91845ee26502316c3
Nakladal, Patrik (Brno) CZE <patrik.nakladal@heidelbergmaterials.com>
2024-08-20 10:05:27 +0200

    fix(get-reply.py): Some issues with handling connection to Serial

--- get-reply.py
+++ get-reply.py
@@ -15,7 +15,7 @@ import serial.tools.list_ports
 
 # Set up logging configuration
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
