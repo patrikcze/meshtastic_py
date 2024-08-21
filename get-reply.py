638fa7462ac05cbe991e2a325432e4540f0e1e5a
Nakladal, Patrik (Brno) CZE <patrik.nakladal@heidelbergcement.com>
2024-08-21 16:43:35 +0200

    chore(get-reply.py): Added myNode Info.

--- get-reply.py
+++ get-reply.py
@@ -517,7 +517,12 @@ def main():
 
     # Subscribe to messages
     pub.subscribe(on_receive, "meshtastic.receive")
-   
+    
+    if interface.nodes:
+        for n in interface.nodes.values():
+            if n["num"] == interface.myInfo.my_node_num:
+                logger.info(f"My Node number is {n['num']} and my user id is {n['user']['id']} - hw model is {n['user']['hwModel']}")
+    
     print("Listening for messages... Press Ctrl+C to stop.")
     try:
         while True:
